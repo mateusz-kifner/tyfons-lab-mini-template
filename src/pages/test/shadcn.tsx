7176a075e72eec6a0c88ee56551b391968dfb14e
Mateusz Kifner <29428687+mateusz-kifner@users.noreply.github.com>
2024-10-30 21:54:52 +0100

    feat: enable shadcn test page in production

--- src/pages/test/shadcn.tsx
+++ src/pages/test/shadcn.tsx
@@ -1,10 +1,10 @@
 "use client";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
 } from "@/components/ui/card";
 import { Suspense } from "react";
 import { cn } from "@/lib/utils";
@@ -15,421 +15,421 @@ import { ThemeToggle } from "@/components/ui/theme";
 import dynamic from "next/dynamic";
 
 const TestAccordion = dynamic(
-  () => import("@/components/test/shadcn/test-accordion"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-accordion"),
+	{ ssr: false },
 );
 const TestAlert = dynamic(() => import("@/components/test/shadcn/test-alert"), {
-  ssr: false,
+	ssr: false,
 });
 const TestAlertDialog = dynamic(
-  () => import("@/components/test/shadcn/test-alert-dialog"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-alert-dialog"),
+	{ ssr: false },
 );
 const TestAspectRatio = dynamic(
-  () => import("@/components/test/shadcn/test-aspect-ratio"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-aspect-ratio"),
+	{ ssr: false },
 );
 const TestBadge = dynamic(() => import("@/components/test/shadcn/test-badge"), {
-  ssr: false,
+	ssr: false,
 });
 const TestBreadcrumb = dynamic(
-  () => import("@/components/test/shadcn/test-breadcrumb"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-breadcrumb"),
+	{ ssr: false },
 );
 const TestButton = dynamic(
-  () => import("@/components/test/shadcn/test-button"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-button"),
+	{ ssr: false },
 );
 const TestCalendar = dynamic(
-  () => import("@/components/test/shadcn/test-calendar"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-calendar"),
+	{ ssr: false },
 );
 
 const TestCard = dynamic(() => import("@/components/test/shadcn/test-card"), {
-  ssr: false,
+	ssr: false,
 });
 const TestCarousel = dynamic(
-  () => import("@/components/test/shadcn/test-carousel"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-carousel"),
+	{ ssr: false },
 );
 const TestChart = dynamic(() => import("@/components/test/shadcn/test-chart"), {
-  ssr: false,
+	ssr: false,
 });
 const TestCheckbox = dynamic(
-  () => import("@/components/test/shadcn/test-checkbox"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-checkbox"),
+	{ ssr: false },
 );
 const TestCollapsible = dynamic(
-  () => import("@/components/test/shadcn/test-collapsible"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-collapsible"),
+	{ ssr: false },
 );
 const TestCombobox = dynamic(
-  () => import("@/components/test/shadcn/test-combobox"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-combobox"),
+	{ ssr: false },
 );
 const TestCommand = dynamic(
-  () => import("@/components/test/shadcn/test-command"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-command"),
+	{ ssr: false },
 );
 const TestContextMenu = dynamic(
-  () => import("@/components/test/shadcn/test-context-menu"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-context-menu"),
+	{ ssr: false },
 );
 const TestDialog = dynamic(
-  () => import("@/components/test/shadcn/test-dialog"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-dialog"),
+	{ ssr: false },
 );
 const TestDrawer = dynamic(
-  () => import("@/components/test/shadcn/test-drawer"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-drawer"),
+	{ ssr: false },
 );
 const TestDropdownMenu = dynamic(
-  () => import("@/components/test/shadcn/test-dropdown-menu"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-dropdown-menu"),
+	{ ssr: false },
 );
 const TestForm = dynamic(() => import("@/components/test/shadcn/test-form"), {
-  ssr: false,
+	ssr: false,
 });
 const TestHoverCard = dynamic(
-  () => import("@/components/test/shadcn/test-hover-card"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-hover-card"),
+	{ ssr: false },
 );
 const TestInputOTP = dynamic(
-  () => import("@/components/test/shadcn/test-input-otp"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-input-otp"),
+	{ ssr: false },
 );
 const TestInput = dynamic(() => import("@/components/test/shadcn/test-input"), {
-  ssr: false,
+	ssr: false,
 });
 const TestLabel = dynamic(() => import("@/components/test/shadcn/test-label"), {
-  ssr: false,
+	ssr: false,
 });
 const TestMenubar = dynamic(
-  () => import("@/components/test/shadcn/test-menubar"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-menubar"),
+	{ ssr: false },
 );
 const TestNavigationMenu = dynamic(
-  () => import("@/components/test/shadcn/test-navigation-menu"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-navigation-menu"),
+	{ ssr: false },
 );
 const TestPagination = dynamic(
-  () => import("@/components/test/shadcn/test-pagination"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-pagination"),
+	{ ssr: false },
 );
 const TestPaginationPrimitive = dynamic(
-  () => import("@/components/test/shadcn/test-pagination-primitive"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-pagination-primitive"),
+	{ ssr: false },
 );
 const TestPopover = dynamic(
-  () => import("@/components/test/shadcn/test-popover"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-popover"),
+	{ ssr: false },
 );
 const TestProgress = dynamic(
-  () => import("@/components/test/shadcn/test-progress"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-progress"),
+	{ ssr: false },
 );
 const TestRatioGroup = dynamic(
-  () => import("@/components/test/shadcn/test-ratio-group"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-ratio-group"),
+	{ ssr: false },
 );
 const TestResizable = dynamic(
-  () => import("@/components/test/shadcn/test-resizable"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-resizable"),
+	{ ssr: false },
 );
 const TestScrollArea = dynamic(
-  () => import("@/components/test/shadcn/test-scroll-area"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-scroll-area"),
+	{ ssr: false },
 );
 const TestSelect = dynamic(
-  () => import("@/components/test/shadcn/test-select"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-select"),
+	{ ssr: false },
 );
 const TestSeparator = dynamic(
-  () => import("@/components/test/shadcn/test-separator"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-separator"),
+	{ ssr: false },
 );
 const TestSheet = dynamic(() => import("@/components/test/shadcn/test-sheet"), {
-  ssr: false,
+	ssr: false,
 });
 const TestSkeleton = dynamic(
-  () => import("@/components/test/shadcn/test-skeleton"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-skeleton"),
+	{ ssr: false },
 );
 const TestSlider = dynamic(
-  () => import("@/components/test/shadcn/test-slider"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-slider"),
+	{ ssr: false },
 );
 const TestSonner = dynamic(
-  () => import("@/components/test/shadcn/test-sonner"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-sonner"),
+	{ ssr: false },
 );
 const TestSwitch = dynamic(
-  () => import("@/components/test/shadcn/test-switch"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-switch"),
+	{ ssr: false },
 );
 const TestTable = dynamic(() => import("@/components/test/shadcn/test-table"), {
-  ssr: false,
+	ssr: false,
 });
 const TestTabs = dynamic(() => import("@/components/test/shadcn/test-tabs"), {
-  ssr: false,
+	ssr: false,
 });
 const TestTextarea = dynamic(
-  () => import("@/components/test/shadcn/test-textarea"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-textarea"),
+	{ ssr: false },
 );
 const TestToggle = dynamic(
-  () => import("@/components/test/shadcn/test-toggle"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-toggle"),
+	{ ssr: false },
 );
 const TestTooltip = dynamic(
-  () => import("@/components/test/shadcn/test-tooltip"),
-  { ssr: false },
+	() => import("@/components/test/shadcn/test-tooltip"),
+	{ ssr: false },
 );
 
 const UIElements: {
-  title: string;
-  description?: string;
-  Element: ComponentType;
-  className?: string;
+	title: string;
+	description?: string;
+	Element: ComponentType;
+	className?: string;
 }[] = [
-  {
-    title: "Accordion",
-    Element: TestAccordion,
-  },
-  {
-    title: "Alert",
-    Element: TestAlert,
-    className: "flex-col",
-  },
-  {
-    title: "AlertDialog",
-    Element: TestAlertDialog,
-  },
-  {
-    title: "AspectRatio",
-    description: "16 / 9",
-    Element: TestAspectRatio,
-  },
-  {
-    title: "Badge",
-    Element: TestBadge,
-  },
-  {
-    title: "Breadcrumb",
-    Element: TestBreadcrumb,
-  },
-  {
-    title: "Button",
-    Element: TestButton,
-    className: "flex-col",
-  },
-  {
-    title: "Calendar",
-    Element: TestCalendar,
-  },
-  {
-    title: "Card",
-    Element: TestCard,
-  },
-  {
-    title: "Carousel",
-    Element: TestCarousel,
-    className: "justify-center",
-  },
-  {
-    title: "Chart",
-    Element: TestChart,
-  },
-  {
-    title: "Checkbox",
-    Element: TestCheckbox,
-    className: "flex-col",
-  },
-  {
-    title: "Collapsible",
-    Element: TestCollapsible,
-    className: "flex-col",
-  },
-  {
-    title: "Combobox",
-    Element: TestCombobox,
-  },
-  {
-    title: "Command",
-    Element: TestCommand,
-  },
-  {
-    title: "ContextMenu",
-    Element: TestContextMenu,
-  },
-  {
-    title: "Dialog",
-    Element: TestDialog,
-  },
-  {
-    title: "Drawer",
-    Element: TestDrawer,
-  },
-  {
-    title: "DropdownMenu",
-    Element: TestDropdownMenu,
-    className: "justify-center",
-  },
+	{
+		title: "Accordion",
+		Element: TestAccordion,
+	},
+	{
+		title: "Alert",
+		Element: TestAlert,
+		className: "flex-col",
+	},
+	{
+		title: "AlertDialog",
+		Element: TestAlertDialog,
+	},
+	{
+		title: "AspectRatio",
+		description: "16 / 9",
+		Element: TestAspectRatio,
+	},
+	{
+		title: "Badge",
+		Element: TestBadge,
+	},
+	{
+		title: "Breadcrumb",
+		Element: TestBreadcrumb,
+	},
+	{
+		title: "Button",
+		Element: TestButton,
+		className: "flex-col",
+	},
+	{
+		title: "Calendar",
+		Element: TestCalendar,
+	},
+	{
+		title: "Card",
+		Element: TestCard,
+	},
+	{
+		title: "Carousel",
+		Element: TestCarousel,
+		className: "justify-center",
+	},
+	{
+		title: "Chart",
+		Element: TestChart,
+	},
+	{
+		title: "Checkbox",
+		Element: TestCheckbox,
+		className: "flex-col",
+	},
+	{
+		title: "Collapsible",
+		Element: TestCollapsible,
+		className: "flex-col",
+	},
+	{
+		title: "Combobox",
+		Element: TestCombobox,
+	},
+	{
+		title: "Command",
+		Element: TestCommand,
+	},
+	{
+		title: "ContextMenu",
+		Element: TestContextMenu,
+	},
+	{
+		title: "Dialog",
+		Element: TestDialog,
+	},
+	{
+		title: "Drawer",
+		Element: TestDrawer,
+	},
+	{
+		title: "DropdownMenu",
+		Element: TestDropdownMenu,
+		className: "justify-center",
+	},
 
-  {
-    title: "Form",
-    Element: TestForm,
-  },
-  {
-    title: "HoverCard",
-    Element: TestHoverCard,
-    className: "justify-center",
-  },
-  {
-    title: "InputOTP",
-    Element: TestInputOTP,
-    className: "flex-col",
-  },
-  {
-    title: "Input",
-    Element: TestInput,
-    className: "flex-col",
-  },
-  {
-    title: "Label",
-    Element: TestLabel,
-    className: "flex-col",
-  },
-  {
-    title: "Menubar",
-    Element: TestMenubar,
-  },
-  {
-    title: "NavigationMenu",
-    Element: TestNavigationMenu,
-  },
-  {
-    title: "Pagination",
-    Element: TestPagination,
-    className: "flex-col",
-  },
-  {
-    title: "Pagination Primitive",
-    Element: TestPaginationPrimitive,
-    className: "flex-col",
-  },
-  {
-    title: "Popover",
-    Element: TestPopover,
-  },
-  {
-    title: "Progress",
-    Element: TestProgress,
-  },
-  {
-    title: "RatioGroup",
-    Element: TestRatioGroup,
-  },
-  {
-    title: "Resizable",
-    Element: TestResizable,
-  },
-  {
-    title: "ScrollArea",
-    Element: TestScrollArea,
-  },
-  {
-    title: "Select",
-    Element: TestSelect,
-    className: "flex-col",
-  },
-  {
-    title: "Separator",
-    Element: TestSeparator,
-  },
-  {
-    title: "Sheet",
-    Element: TestSheet,
-  },
-  {
-    title: "Skeleton",
-    Element: TestSkeleton,
-    className: "flex-col",
-  },
-  {
-    title: "Slider",
-    Element: TestSlider,
-    className: "flex-col gap-7 pb-7",
-  },
-  {
-    title: "Sonner",
-    Element: TestSonner,
-  },
-  {
-    title: "Switch",
-    Element: TestSwitch,
-  },
-  {
-    title: "Table",
-    Element: TestTable,
-  },
-  {
-    title: "Tabs",
-    Element: TestTabs,
-    className: "justify-center",
-  },
-  {
-    title: "TextArea",
-    Element: TestTextarea,
-    className: "flex-col",
-  },
-  {
-    title: "Toggle",
-    Element: TestToggle,
-    className: "flex-col",
-  },
-  {
-    title: "Tooltip",
-    Element: TestTooltip,
-    className: "flex-col",
-  },
+	{
+		title: "Form",
+		Element: TestForm,
+	},
+	{
+		title: "HoverCard",
+		Element: TestHoverCard,
+		className: "justify-center",
+	},
+	{
+		title: "InputOTP",
+		Element: TestInputOTP,
+		className: "flex-col",
+	},
+	{
+		title: "Input",
+		Element: TestInput,
+		className: "flex-col",
+	},
+	{
+		title: "Label",
+		Element: TestLabel,
+		className: "flex-col",
+	},
+	{
+		title: "Menubar",
+		Element: TestMenubar,
+	},
+	{
+		title: "NavigationMenu",
+		Element: TestNavigationMenu,
+	},
+	{
+		title: "Pagination",
+		Element: TestPagination,
+		className: "flex-col",
+	},
+	{
+		title: "Pagination Primitive",
+		Element: TestPaginationPrimitive,
+		className: "flex-col",
+	},
+	{
+		title: "Popover",
+		Element: TestPopover,
+	},
+	{
+		title: "Progress",
+		Element: TestProgress,
+	},
+	{
+		title: "RatioGroup",
+		Element: TestRatioGroup,
+	},
+	{
+		title: "Resizable",
+		Element: TestResizable,
+	},
+	{
+		title: "ScrollArea",
+		Element: TestScrollArea,
+	},
+	{
+		title: "Select",
+		Element: TestSelect,
+		className: "flex-col",
+	},
+	{
+		title: "Separator",
+		Element: TestSeparator,
+	},
+	{
+		title: "Sheet",
+		Element: TestSheet,
+	},
+	{
+		title: "Skeleton",
+		Element: TestSkeleton,
+		className: "flex-col",
+	},
+	{
+		title: "Slider",
+		Element: TestSlider,
+		className: "flex-col gap-7 pb-7",
+	},
+	{
+		title: "Sonner",
+		Element: TestSonner,
+	},
+	{
+		title: "Switch",
+		Element: TestSwitch,
+	},
+	{
+		title: "Table",
+		Element: TestTable,
+	},
+	{
+		title: "Tabs",
+		Element: TestTabs,
+		className: "justify-center",
+	},
+	{
+		title: "TextArea",
+		Element: TestTextarea,
+		className: "flex-col",
+	},
+	{
+		title: "Toggle",
+		Element: TestToggle,
+		className: "flex-col",
+	},
+	{
+		title: "Tooltip",
+		Element: TestTooltip,
+		className: "flex-col",
+	},
 ];
 
 function ShadCN() {
-  // const { toggleTheme, theme } = useUserContext();
-  const uuid = useId();
-  console.log("test");
-  return (
-    <div className="mx-auto flex max-w-screen-xl flex-col gap-4 p-2 pb-96">
-      <ThemeToggle />
-      {UIElements.map((val, index) => (
-        <Card key={`${uuid}${index}:`}>
-          <CardHeader>
-            <CardTitle>{val.title}</CardTitle>
-            {val.description !== undefined && (
-              <CardDescription>{val.description}</CardDescription>
-            )}
-          </CardHeader>
-          <CardContent className={cn("flex gap-2 p-2", val.className)}>
-            <Suspense
-              fallback={
-                <IconLoader2 className="direction-reverse animate-spin" />
-              }
-            >
-              <val.Element />
-            </Suspense>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
-  );
+	// const { toggleTheme, theme } = useUserContext();
+	const uuid = useId();
+	console.log("test");
+	return (
+		<div className="mx-auto flex max-w-screen-xl flex-col gap-4 p-2 pb-96">
+			<ThemeToggle />
+			{UIElements.map((val, index) => (
+				<Card key={`${uuid}${index}:`}>
+					<CardHeader>
+						<CardTitle>{val.title}</CardTitle>
+						{val.description !== undefined && (
+							<CardDescription>{val.description}</CardDescription>
+						)}
+					</CardHeader>
+					<CardContent className={cn("flex gap-2 p-2", val.className)}>
+						<Suspense
+							fallback={
+								<IconLoader2 className="direction-reverse animate-spin" />
+							}
+						>
+							<val.Element />
+						</Suspense>
+					</CardContent>
+				</Card>
+			))}
+		</div>
+	);
 }
 
 export default ShadCN;
 
-export const getStaticProps: GetStaticProps = () => {
-  if (process.env.NODE_ENV === "production") {
-    return { notFound: true };
-  }
-  return { props: {} };
-};
+// export const getStaticProps: GetStaticProps = () => {
+//   if (process.env.NODE_ENV === "production") {
+//     return { notFound: true };
+//   }
+//   return { props: {} };
+// };
